d7ba6eaac957c2784941665e25338166f8fe4bb4
Galiks <31892748+Galiks@users.noreply.github.com>
2019-08-21 02:29:10 +0400

    added Task 7

--- Regex/Program.cs
+++ Regex/Program.cs
@@ -1,15 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
-namespace Regex
+namespace RegexTask
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            #region Task 1
+
+            Console.WriteLine("Enter the text: ");
+            string userText = Console.ReadLine();
+            if (Task1.IsStringHasDate(userText))
+            {
+                Console.WriteLine($"Text \"{userText}\" has date");
+            }
+            else
+            {
+                Console.WriteLine($"Text \"{userText}\" don't has date");
+            }
+
+            #endregion Task 1
+
+            #region Task 3
+
+            string textWithEmails = "Иван: ivan@mail.ru, Петр: p_ivanov@mail.rol.ru";
+            var emails = Task3.GetEmail(textWithEmails);
+            foreach (var item in emails)
+            {
+                Console.WriteLine(item);
+            }
+
+            #endregion Task 3
+
+            #region Task 2
+
+            string textWithTags = "<b>Это</b> текст <i>с</i> <font color=\"red\">HTML</font> кодами ";
+            string textWithoutTags = Task2.GetTextWithoutHtmlTags(textWithTags);
+            Console.WriteLine(textWithoutTags);
+
+            #endregion Task 2
+
+            Console.Read();
         }
     }
 }
---
+++ Regex/Task1.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegexTask
+{
+    public static class Task1
+    {
+        private const string patternDate = @"\d{2}[\\\/.-]\d{2}[\\\/.-]\d{4}";
+
+        public static bool IsStringHasDate(string date)
+        {
+            if (Regex.IsMatch(date, patternDate))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
