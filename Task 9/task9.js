936832b0a110849eb8d86c9f335375254de1fe46
Galiks <31892748+Galiks@users.noreply.github.com>
2019-08-29 01:22:30 +0400

    Task 9 - 2

--- Task 9/task9.js
+++ Task 9/task9.js
@@ -25,7 +25,7 @@ function charRemover() {
     document.getElementById("task1-output").value = result;
 }
     
-    function splitMulti(str, separators) {
+function splitMulti(str, separators) {
       for (var i = 0; i < str.length; i++) {
         if (separators.indexOf(str[i]) !== -1) {
           str = str.replace(str[i], " ");
@@ -38,4 +38,64 @@ function charRemover() {
           return word;
         }
       })
-    }
+}
+
+var error = "ERROR";
+
+//var massife = "5????+5???????*??4????????/??2????????=".match(/\d+(\.\d+)?|([+\-*/=])/g);
+    
+function mathCalculator() {
+      var massife = document
+        .getElementById("task2-input")
+        .value.match(/\d+(\.\d+)?|([+\-*/=])/g);
+    
+      var result;
+    
+      check(massife);
+    
+      var operations = {
+        "+": function(x, y) {
+          return x + y;
+        },
+    
+        "-": function(x, y) {
+          return x - y;
+        },
+    
+        "*": function(x, y) {
+          return x * y;
+        },
+    
+        "/": function(x, y) {
+          return y !== 0 ? x / y : 0;
+        }
+      };
+    
+      result = massife[0];
+      for (var i = 1; i + 2 < massife.length; i += 2) {
+        result = operations[massife[i]](Number(result), Number(massife[i + 1])).toFixed(2);
+      }
+    
+      document.getElementById("task2-output").value = result;
+}
+    
+function check(massife) {
+      if (
+        massife.filter(function(x) {
+          return x === "=";
+        }).length !== 1 &&
+        massife[massife.lastIndex] !== "="
+      ) {
+        document.getElementById("task2-output").value = error;
+      }
+      for (let i = 0; i < massife.length; i++) {
+        if (i % 2 === 0 && isNaN(massife[i])) {
+          document.getElementById("task2-output").value = error;
+        }
+        if (i % 2 === 1 && !isNaN(massife[i])) {
+          document.getElementById("task2-output").value = error;
+        }
+      }
+      return true;
+}
+    
