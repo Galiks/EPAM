1b5da23c3067b77a4fc83d3eb98f1059f5e67f8e
Galiks <31892748+Galiks@users.noreply.github.com>
2019-10-01 23:35:23 +0400

    Добавил реализацию для GradeDAO. Добавил в БД строку для фото пользователя

---
+++ CalendarThematicPlan.DAL.DAO/GradeDao.cs
@@ -0,0 +1,182 @@
+﻿using CalendarThematicPlan.DAL.Interface;
+using CalendarThematicPlan.Entity;
+using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CalendarThematicPlan.DAL.DAO
+{
+    public class GradeDao : IGradeDao
+    {
+        private readonly string connectionString;
+
+        public GradeDao()
+        {
+            this.connectionString = ConfigurationManager.ConnectionStrings["CTP"].ConnectionString;
+        }
+
+        public int? AddGrade(Grade grade)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var command = connection.CreateCommand();
+                command.CommandText = "AddGrade";
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddRange(new[]
+                {
+                    new SqlParameter
+                    {
+                        ParameterName = "@Number",
+                        Value = grade.Number,
+                        SqlDbType = SqlDbType.Int,
+                        Direction = ParameterDirection.Input
+                    },
+
+                    new SqlParameter
+                    {
+                        ParameterName = "@Letter",
+                        Value = grade.Letter,
+                        SqlDbType = SqlDbType.NVarChar,
+                        Direction = ParameterDirection.Input
+                    },
+
+                    new SqlParameter
+                    {
+                        ParameterName = "@KidsInClass",
+                        Value = grade.KidsInClass,
+                        SqlDbType = SqlDbType.Int,
+                        Direction = ParameterDirection.Input
+                    },
+                });
+
+                var id = new SqlParameter
+                {
+                    ParameterName = "@Id_output",
+                    SqlDbType = SqlDbType.Int,
+                    Direction = ParameterDirection.Output,
+                };
+
+                connection.Open();
+
+                return (int?)id.Value;
+            }
+        }
+
+        public void DeleteGrade(int id)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var command = connection.CreateCommand();
+                command.CommandText = "DeleteGrade";
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddRange(new[]
+                {
+                    new SqlParameter
+                    {
+                        ParameterName = "@Id",
+                        Value = id,
+                        SqlDbType = SqlDbType.Int,
+                        Direction = ParameterDirection.Input
+                    },
+
+                });
+
+                connection.Open();
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public Grade GetGradeById(int id)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var command = connection.CreateCommand();
+                command.CommandText = "GetGradeById";
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddRange(new[]
+                {
+                    new SqlParameter
+                    {
+                        ParameterName = "@Id",
+                        Value = id,
+                        SqlDbType = SqlDbType.Int,
+                        Direction = ParameterDirection.Input
+                    },
+
+                });
+
+                connection.Open();
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        return new Grade
+                        {
+                            Id = id,
+                            Number = (int)reader["Number"],
+                            Letter = (string)reader["Letter"],
+                            KidsInClass = (int)reader["KidsInClass"]
+                        };
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        public void UpdateGrade(Grade grade)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var command = connection.CreateCommand();
+                command.CommandText = "UpdateGrade";
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddRange(new[]
+                {
+                    new SqlParameter
+                    {
+                        ParameterName = "@Id",
+                        Value = grade.Id,
+                        SqlDbType = SqlDbType.Int,
+                        Direction = ParameterDirection.Input
+                    },
+
+                    new SqlParameter
+                    {
+                        ParameterName = "@Number",
+                        Value = grade.Number,
+                        SqlDbType = SqlDbType.Int,
+                        Direction = ParameterDirection.Input
+                    },
+
+                    new SqlParameter
+                    {
+                        ParameterName = "@Letter",
+                        Value = grade.Letter,
+                        SqlDbType = SqlDbType.NVarChar,
+                        Direction = ParameterDirection.Input
+                    },
+
+                    new SqlParameter
+                    {
+                        ParameterName = "@KidsInClass",
+                        Value = grade.KidsInClass,
+                        SqlDbType = SqlDbType.Int,
+                        Direction = ParameterDirection.Input
+                    },
+                });
+
+                connection.Open();
+
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ CalendarThematicPlan.DAL.DAO/ScheduleDao.cs
@@ -0,0 +1,41 @@
+﻿using CalendarThematicPlan.DAL.Interface;
+using CalendarThematicPlan.Entity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarThematicPlan.DAL.DAO
+{
+    public class ScheduleDao : IScheduleDao
+    {
+        private readonly string connectionString;
+
+        public ScheduleDao()
+        {
+            this.connectionString = ConfigurationManager.ConnectionStrings["CTP"].ConnectionString;
+        }
+
+        public int? AddSchedule(Schedule grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteSchedule(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Schedule GetScheduleById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateSchedule(Schedule grade)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
