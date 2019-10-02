0c175f8412db228ec56d09ec14a13136f79dc1e6
Galiks <31892748+Galiks@users.noreply.github.com>
2019-10-02 15:58:38 +0400

    Изменил сущности Schedule и Subject добавлением полей

--- CalendarThematicPlan.Entity/Schedule.cs
+++ CalendarThematicPlan.Entity/Schedule.cs
@@ -10,9 +10,12 @@ namespace CalendarThematicPlan.Entity
     {
         public int? Id { get; set; }
         public DateTime Date { get; set; }
+        public DateTime ActualDate { get; set; }
         public string Room { get; set; }
         public int IdSubject { get; set; }
         public int IdGrade { get; set; }
         public int IdUser { get; set; }
+        public string LessonTopic { get; set; }
+        public string Comment { get; set; }
     }
 }
--- CalendarThematicPlan.Entity/Subject.cs
+++ CalendarThematicPlan.Entity/Subject.cs
@@ -10,6 +10,6 @@ namespace CalendarThematicPlan.Entity
     {
         public int? Id { get; set; }
         public string Name { get; set; }
-
+        public int Hours { get; set; }
     }
 }
