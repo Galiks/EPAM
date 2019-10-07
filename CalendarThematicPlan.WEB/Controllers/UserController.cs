63745d0098a3afe19c3c5682209cb965ccadf6e7
Galiks <31892748+Galiks@users.noreply.github.com>
2019-10-07 11:31:29 +0400

    Details работает! ВОУ!

--- CalendarThematicPlan.WEB/Controllers/UserController.cs
+++ CalendarThematicPlan.WEB/Controllers/UserController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CalendarThematicPlan.BLL.Interface;
+using CalendarThematicPlan.Container;
+using Ninject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +11,13 @@ namespace CalendarThematicPlan.WEB.Controllers
 {
     public class UserController : Controller
     {
+        private readonly IUserLogic userLogic;
+
+        public UserController()
+        {
+            this.userLogic = NinjectCommon.Kernel.Get<IUserLogic>();
+        }
+
         // GET: User
         public ActionResult Index()
         {
@@ -27,6 +37,12 @@ namespace CalendarThematicPlan.WEB.Controllers
         public ActionResult Delete()
         {
             return View();
+        }
+
+        public ActionResult Details(int? idUser)
+        {
+            var user = userLogic.GetUserById(idUser.ToString());
+            return View(user);
         }
     }
 }
